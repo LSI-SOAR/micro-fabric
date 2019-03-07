d3d1fe74d81d1f73c20698800fd899b5a7db0235
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2019-03-07 13:35:02 -0500

    Adding config class; logging cleanup;

--- micro-fabric.js
+++ micro-fabric.js
@@ -2,6 +2,7 @@
 const utils = require('./lib/utils');
 const Module = require('./lib/module');
 const Application = require('./lib/application');
+const Config = require('./lib/config');
 const Network = require('./lib/network');
 const PopupProxy = require('./lib/popup-proxy');
 const Popup = require('./lib/popup');
@@ -21,6 +22,7 @@ module.exports = {
     Module, 
     Application, 
     Network,
+    Config,
     utils,
     PopupProxy,
     Popup
